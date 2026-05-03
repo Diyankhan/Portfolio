511c6b4c8c34943d4387c8209464ca3ec1a85146
Diyan khan <diyankhan33@gmail.com>
2026-05-03 17:19:23 +0530

    Solving error

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,21 +1,28 @@
 const transporter = require("../config/nodemailer");
+const expressError = require("../expressError");
 const { mailValidation } = require("../middleware/validation");
 const wrapAsync = require("../middleware/wrapAsync");
 exports.sendMail = wrapAsync(async (req, res) => {
-  await mailValidation.validateAsync(req.body);
-  let { name, lastName, mail, sub, msg } = req.body;
-  const info = await transporter.sendMail({
-    from: `${name} <diyankhan33@gmail.com>`, // Visible From: header
-    to: "diyankhan33@gmail.com",
-    replyTo: mail, // Visible To: header
-    subject: sub,
-    text: `Name : ${name} ${lastName} \n
-             From : ${mail} \n 
-             Message : ${msg}
-      `,
-  });
-  return res.status(201).json({
-    success: true,
-    message: "Mail sent successfully",
-  });
+  try {
+    await mailValidation.validateAsync(req.body);
+
+    let { name, lastName, mail, sub, msg } = req.body;
+
+    await transporter.sendMail({
+      from: `${name} <diyankhan33@gmail.com>`,
+      to: "diyankhan33@gmail.com",
+      replyTo: mail,
+      subject: sub,
+      text: `Name: ${name} ${lastName}\nFrom: ${mail}\nMessage: ${msg}`,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Mail sent successfully",
+    });
+
+  } catch (err) {
+    console.log("REAL ERROR:", err); 
+    throw new expressError(500, "Mail failed")
+  }
 });
